22aa769ec9f5f7e518ddcdaa161ec187151fd702
direyesm <37608963+direyesm@users.noreply.github.com>
2020-08-31 23:08:02 -0400

    Initial commit

---
+++ app/src/main/java/com/example/kotlinexample/Main.kt
@@ -0,0 +1,69 @@
+package com.example.kotlinexample
+
+//// Ejercicio 01
+fun main(){
+
+////    Ejercico 02
+//    val nombre = "Diego"
+//    println("Mi Nombres: ${nombre}")
+
+////    Ejercicio 03
+//    var num = 10
+//    var num1 = 20
+//    var num2 = 30
+//
+////    Ejercicio 04
+//    var suma =  num + num1 + num2
+//    println("la suma de lso numero es: ${suma}")
+
+
+////    Ejercicio 05 y 06
+//    var string = "Ark Reigen"
+//    var char = 'A'
+
+//// Ejercicio 07
+//
+//    char = 'B'
+//
+//    println("El valor de la palabra es : ${string.length}")
+
+////    Ejercicio 08
+//
+//    var num = 0F
+//
+//
+////   Ejercicio 09
+//
+//    num = 8.3f
+//
+////    Ejercicio 10
+//
+//    println(Byte.MAX_VALUE)
+//    println(Short.MAX_VALUE)
+
+//    Ejercicio 11
+//
+//    println(Int.MIN_VALUE)
+//    println(Long.MIN_VALUE)
+
+////    Ejercicio 12
+//
+//    var boolean = true
+//
+//    println("El valor es: ${boolean}")
+
+//    fun imprimiendoParametros(palabra : String, palabra1 : String){
+
+//
+//
+//    }
+
+////    Ejericcio 13
+//    fun obtieneIVA(iva : Float): Float{
+//        return 19
+
+//    }
+
+}
+
+
